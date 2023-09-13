2a75a5c3dbc54c39724e03c1c818b2dc876a485d
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-09-13 11:16:12 +0100

    CarCard

--- components/Index.ts
+++ components/Index.ts
@@ -5,6 +5,8 @@ import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
 
 export {
@@ -15,4 +17,6 @@ export {
     SearchBar,
     CustomFilter,
     SearchManufacturer,
+    CarCard,
+    CarDetails,
 }
